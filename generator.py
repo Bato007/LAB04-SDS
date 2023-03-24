5758fbefd9c9339c8708b23c2147753c383dcb32
Brandon <brandonhernandez_10@outlook.es>
2023-03-24 10:56:40 -0600

    feat: fix generator and data

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-import os, pefile
+import os, pefile, subprocess
 import pandas as pd
 
 malware_path = './MALWR'
@@ -6,7 +6,7 @@ malware_path = './MALWR'
 class Section(object):
   def __init__(self, name, virtualAdress, virtualSize, rawDataSize) -> None:
     self.name = name
-    self.virtualAdress = virtualAdress
+    self.virtualAddress = virtualAdress
     self.virtualSize = virtualSize
     self.rawDataSize = rawDataSize
 
@@ -18,50 +18,64 @@ class Malware(object):
     self.dll = dll
     self.date = date
 
+available = ['.text', '.data', '.rscr']
 malwares = []
 for i, filename in enumerate(os.listdir(malware_path)):
   if (filename == '.DS_Store'): continue
+
   full_path = malware_path + '/' + filename
-  print('\n[GENERATOR]: Reading...', full_path)
+  print('\n[GENERATOR]: Reading...', filename)
+
+  subprocess.run(['upx', '-d', full_path])
 
   pe = pefile.PE(full_path)
 
   # Reading the sections
   sections = []
-  print('[GENERATOR]: Printing sections')
+  virtualAddress = []
+  virtualSize = []
+  rawDataSize = []
   for section in pe.sections:
-    sectionInfo = Section(section.name, hex(section.VirtualAddress), hex(section.Misc_VirtualSize), section.SizeOfRawData)
-    sections.append(sectionInfo)
-    print(sectionInfo.name, sectionInfo.virtualAdress, sectionInfo.virtualSize, sectionInfo.rawDataSize)
+    sectionName = str(section.Name.decode('utf-8')).rstrip('\x00')
+    sections.append(sectionName)
+    virtualAddress.append(hex(section.VirtualAddress))
+    virtualSize.append(hex(section.Misc_VirtualSize))
+    rawDataSize.append(section.SizeOfRawData)
+  print('[GENERATOR]: Getting sections', sections)
+  section = Section(sections, virtualAddress, virtualSize, rawDataSize)
 
   # Reading the dll and function calls
   dllCalls = []
   functionsCalled = []
+  print('[GENERATOR]: DLL calls and functions')
   for entry in pe.DIRECTORY_ENTRY_IMPORT:
-    print('[GENERATOR]: DLL calls\t', entry.dll)
-
-    print('[GENERATOR]: Functions')
+    dllCalls.append(entry.dll.decode('utf-8'))
     for function in entry.imports:
       functionsCalled.append(function.name.decode('utf-8'))
-      print('\t', function.name)
-    dllCalls.append(entry.dll.decode('utf-8'))
 
   date = pe.FILE_HEADER.dump_dict()['TimeDateStamp']['Value'].split('[')[1][:-1]
-  print('Time Data Stamp:', date)
-  print('Time Data Stamp:', hex(pe.FILE_HEADER.TimeDateStamp))
-  malware = Malware(i, filename, sections, [dllCalls, functionsCalled], date)
+  print('[GENERATOR]: Time Data Stamp:', date)
+  malware = Malware(i, filename, section, [dllCalls, functionsCalled], date)
   malwares.append(malware)
 
 # Now creates the csv
-columns = ['index', 'filename', 'sections', 'dll', 'functions', 'date']
+print('\n[GENERATOR]: Processing...')
+columns = ['index', 'filename', 'sectionName', 'virtualAddress', 'virtualSize', 'rawDataSize', 'dll', 'functions', 'date']
 df = pd.DataFrame(columns=columns)
+
 for malware in malwares:
+  df.loc[malware.index+1] = [
+    malware.index,
+    malware.filename,
+    malware.sections.name,
+    malware.sections.virtualAddress,
+    malware.sections.virtualSize,
+    malware.sections.rawDataSize,
+    malware.dll[0],
+    malware.dll[1],
+    malware.date
+  ]
 
-  sections = []
-  for section in malware.sections:
-    sections.append([section.name, section.virtualAdress, section.virtualSize, section.rawDataSize])
-
-  df.loc[malware.index+1] = [malware.index, malware.filename, sections, malware.dll[0], malware.dll[1], malware.date]
 
 df.to_csv('./data.csv', index=False)
 print('[GENERATOR]: Finished')
